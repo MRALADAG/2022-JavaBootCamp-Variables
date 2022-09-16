6551f9052f3a153686c5b263f2054e19c5da2464
MRALADAG <mstf_aladag@yahoo.com>
2022-09-17 01:20:47 +0300

    Performed exercises about some variables in Java

---
+++ variables/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ogrenciSayisi = 13;
+//        System.out.println("Öğrenci sayım: " + ogrenciSayisi);
+        String mesaj = "Öğrenci sayım: ";
+        System.out.println(mesaj + ogrenciSayisi);
+
+    }
+}
